c20dca6158d2f8bbb4b7e0e30330011d6cc97198
Kevin Ford <fordkev@gmail.com>
2014-03-24 16:50:21 -0700

    started templates for user blog pages

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+class BlogsController < ApplicationController
+  
+  
+  def index
+    @user = User.find_by( :username => params[:username])
+    @blog = {}
+    @blog['title'] = @user.username. + "'s Classable Blog!"
+    @blog['meta-title'] = @user.username + "'s HTML Safe Classable Blog Title!"
+    @posts = @user.posts
+    render 'blogs/default/index', :layout => false
+  end
+  
+  def post
+    
+  end
+  
+  def page
+    
+  end
+  
+  private
+  
+end
